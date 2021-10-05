cd4ed284db288a8dd7df03a240ca667a5729b165
Jay Lee <jay.lee.2280@gmail.com>
2021-10-05 20:50:10 +0900

    ➕ Other's solution

--- programmers_level1/additoin-of-matrix.js
+++ programmers_level1/additoin-of-matrix.js
@@ -13,3 +13,20 @@ function solution(arr1, arr2) {
   }
   return answer;
 }
+
+// Other's solution 1
+function sumMatrix(A, B) {
+  return A.map((a, i) => a.map((b, j) => b + B[i][j]));
+}
+
+// Other's solution 2
+function solution2(arr1, arr2) {
+  var answer = [[]];
+  for (let i = 0; i < arr1.length; i++) {
+    answer[i] = [];
+    for (let j = 0; j < arr1[0].length; j++) {
+      answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+  return answer;
+}
