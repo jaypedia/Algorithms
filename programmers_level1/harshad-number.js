a6bdd7dd455ea40c4caf04fe216f0019b02b140c
Jay Lee <jay.lee.2280@gmail.com>
2021-10-05 15:00:46 +0900

    📝 My solution

---
+++ programmers_level1/harshad-number.js
@@ -0,0 +1,12 @@
+// 2021-10-05
+// Harshad number
+
+// My solution
+function solution(arr) {
+  const str = arr.toString();
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
+  }
+  return Number(str) % sum ? false : true;
+}
