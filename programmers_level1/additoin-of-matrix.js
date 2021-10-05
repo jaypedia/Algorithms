3e04f01865411bb29770cb72c2dd3d8bb056cf42
Jay Lee <jay.lee.2280@gmail.com>
2021-10-05 20:28:55 +0900

    📝 My solution

---
+++ programmers_level1/additoin-of-matrix.js
@@ -0,0 +1,15 @@
+// 2021-10-04~5
+// 행렬의 덧셈
+
+// My Solution
+function solution(arr1, arr2) {
+  let answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let temp = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      temp.push(arr1[i][j] + arr2[i][j]);
+    }
+    answer.push(temp);
+  }
+  return answer;
+}
