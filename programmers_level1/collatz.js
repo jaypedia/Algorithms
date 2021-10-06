7732e04d4e77537cfc304b3ca03bcc6550de2144
Jay Lee <jay.lee.2280@gmail.com>
2021-10-06 15:51:11 +0900

    ➕ Other's solution

--- programmers_level1/collatz.js
+++ programmers_level1/collatz.js
@@ -11,3 +11,34 @@ function solution(num) {
   }
   return answer;
 }
+
+// Other's solution 1
+function solution(num) {
+  let answer = 0;
+  while (num !== 1 && answer !== 500) {
+    num % 2 ? (num = num * 3 + 1) : (num = num / 2);
+    answer++;
+  }
+  return num === 1 ? answer : -1;
+}
+
+// Other's solution 2 : Recursive function
+function solution(num, count = 0) {
+  return num === 1
+    ? count >= 500
+      ? -1
+      : count
+    : solution(num % 2 === 0 ? num / 2 : num * 3 + 1, ++count);
+}
+
+// Other's solution 3
+const solution = num => collatzGuessCount(num, 0);
+
+const collatzGuessCount = (num, acc) =>
+  num === 1
+    ? acc > 500
+      ? -1
+      : acc
+    : collatzGuessCount(processCollatz(num), acc + 1);
+
+const processCollatz = num => (num % 2 === 0 ? num / 2 : num * 3 + 1);
