a9f5738833c4839122a7c5d9dc614323227acd96
Jay <jay.lee.2280@gmail.com>
2021-10-10 09:19:58 +0900

    ➕ Other's solution

--- programmers_level1/sum-between-two-integers.js
+++ programmers_level1/sum-between-two-integers.js
@@ -16,3 +16,23 @@ function solution(a, b) {
 
   return sum;
 }
+
+// Other's solution 1
+function adder(a, b) {
+  return ((a + b) * (Math.abs(b - a) + 1)) / 2;
+}
+
+// Other's solution 2
+function adder(a, b, s = 0) {
+  for (var i = Math.min(a, b); i <= Math.max(a, b); i++) s += i;
+  return s;
+}
+
+// Other's solution 3
+function adder(a, b) {
+  var result = a < b ? a : b;
+  while (a != b) {
+    result += a < b ? ++a : ++b;
+  }
+  return result;
+}
