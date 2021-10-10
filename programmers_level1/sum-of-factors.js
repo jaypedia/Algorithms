4af13aedaa6f1d40a8a5c3c92fc64d7134e8a3aa
Jay <jay.lee.2280@gmail.com>
2021-10-10 09:10:17 +0900

    ➕ Other's solution

--- programmers_level1/sum-of-factors.js
+++ programmers_level1/sum-of-factors.js
@@ -12,4 +12,18 @@ function solution(n) {
   return sum;
 }
 
-// Other's solution
+// Other's solution 1
+function solution(n, a = 0, b = 0) {
+  return n <= a / 2 ? b : solution(n, a + 1, (b += n % a ? 0 : a));
+}
+
+// Other's solution 2
+let sumDivisor = num => {
+  let answer = 0,
+    i = 1,
+    j = num % 2 == 0 ? 1 : 2;
+
+  for (i; i < num; i = i + j) if (num % i == 0) answer += i;
+
+  return answer + num;
+};
