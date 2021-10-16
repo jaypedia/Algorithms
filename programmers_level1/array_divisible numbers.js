20f7a1176887efed2fd9877b499019e8f8ed310e
Jay <jay.lee.2280@gmail.com>
2021-10-16 13:36:53 +0900

    ➕ Other's solutions

--- programmers_level1/array_divisible numbers.js
+++ programmers_level1/array_divisible numbers.js
@@ -14,5 +14,33 @@ function solution(arr, divisor) {
 // My solution 2
 function solution(arr, divisor) {
   const newarr = arr.filter(v => v % divisor === 0);
-  newarr.length === 0 ? [-1] : newarr.sort((a, b) => a - b);
+  return newarr.length === 0 ? [-1] : newarr.sort((a, b) => a - b);
+}
+
+// Other's solution 1
+// map(), && operator
+function solution(arr, divisor) {
+  let answer = [];
+  arr.map(v => v % divisor === 0 && answer.push(v));
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
+}
+
+// Other's solution 2
+function solution(arr, divisor) {
+  var answer = [];
+  for (var i = 0; i < arr.length; i++)
+    if (arr[i] % divisor == 0) answer.push(arr[i]);
+
+  if (answer.length == 0) answer = [-1];
+  else
+    answer = answer.sort(function (a, b) {
+      return a - b;
+    });
+  return answer;
+}
+
+// Other's solution 3
+function solution(arr, divisor) {
+  var _ = arr.filter(e => !(e % divisor));
+  return _[0] ? _.sort(($, _) => $ - _) : [-1];
 }
