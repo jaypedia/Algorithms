013430ae267b696722e7d1021eff84640b67ab22
Jay <jay.lee.2280@gmail.com>
2021-10-17 21:21:35 +0900

    ➕ Other's solutions

--- programmers_level1/find_a_number_remainer_1.js
+++ programmers_level1/find_a_number_remainer_1.js
@@ -9,3 +9,33 @@ function solution(n) {
     }
   }
 }
+
+// Other's solution 1
+function solution(n) {
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    if (answer > 0) {
+      break;
+    }
+
+    if (n % i == 1) {
+      answer = i;
+    }
+  }
+
+  return answer;
+}
+
+// Other's solution 2
+function solution(n, x = 1) {
+  while (x++) {
+    if (n % x === 1) return x;
+  }
+}
+
+// Other's solution 3
+function solution(n) {
+  let answer = 1;
+  while (n % answer != 1) answer++;
+  return answer;
+}
