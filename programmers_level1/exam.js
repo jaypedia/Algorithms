6464b644762b807d1fb625e6543ef5f13afa69ec
Jay <jay.lee.2280@gmail.com>
2021-10-30 23:47:07 +0900

    ➕ Other's solutions

--- programmers_level1/exam.js
+++ programmers_level1/exam.js
@@ -20,3 +20,79 @@ function solution(answer) {
 
   return answer;
 }
+
+function solution(answers) {
+  var answer = [];
+  const man1 = [1, 2, 3, 4, 5];
+  const man2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let count = [0, 0, 0];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] == man1[i % man1.length]) count[0]++;
+    if (answers[i] == man2[i % man2.length]) count[1]++;
+    if (answers[i] == man3[i % man3.length]) count[2]++;
+  }
+
+  const max = Math.max(count[0], count[1], count[2]);
+  for (let i = 0; i < count.length; i++) {
+    if (max == count[i]) answer.push(i + 1);
+  }
+
+  return answer;
+}
+
+function solution(answers) {
+  const A = '12345'.split('');
+  const B = '21232425'.split('');
+  const C = '3311224455'.split('');
+
+  const scores = [A, B, C]
+    .map(p => {
+      return answers
+        .map((answer, i) => answer === Number(p[i % p.length]))
+        .filter(c => c).length;
+    })
+    .map((score, i) => ({ id: i + 1, score }))
+    .sort((a, b) => b.score - a.score);
+
+  const ret = [];
+
+  while (scores.length) {
+    const p = scores.shift();
+    if (ret[0] && ret[0].score !== p.score) break;
+    ret.push(p);
+  }
+
+  return ret.map(p => p.id).sort();
+}
+
+function solution(answers) {
+  var answer = [];
+  var user = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+  var point = [0, 0, 0];
+
+  for (var i = 0; i < answers.length; i++) {
+    if (user[0][i % 5] == answers[i]) {
+      point[0]++;
+    }
+    if (user[1][i % 8] == answers[i]) {
+      point[1]++;
+    }
+    if (user[2][i % 10] == answers[i]) {
+      point[2]++;
+    }
+  }
+
+  var max = Math.max.apply(null, point);
+
+  for (var i = 0; i < 3; i++) {
+    if (point[i] == max) answer.push(i + 1);
+  }
+
+  return answer;
+}
