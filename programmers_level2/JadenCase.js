c2a495d93d9293fa7748acaffa69fc26faa76fd6
Jay <jay.lee.2280@gmail.com>
2021-11-05 21:49:53 +0900

    ➕ Other's solutions(2)

--- programmers_level2/JadenCase.js
+++ programmers_level2/JadenCase.js
@@ -12,3 +12,24 @@ function solution(s) {
 }
 
 console.log(solution('3people unFollowed me')); // 3people Unfollowed Me
+
+// Other's solution 1
+function solution(s) {
+  return s
+    .split(' ')
+    .map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
+    .join(' ');
+}
+
+// Other's solution 2
+function solution(s) {
+  var answer = '';
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || s[i - 1] === ' ') {
+      answer += s[i].toUpperCase();
+    } else {
+      answer += s[i].toLowerCase();
+    }
+  }
+  return answer;
+}
