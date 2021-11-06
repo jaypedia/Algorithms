168c634d31e3794768f7ce2724258d7b246f5849
Jay <jay.lee.2280@gmail.com>
2021-11-06 22:53:36 +0900

    ➕ Other solutions(2)

--- programmers_level2/n_lcm.js
+++ programmers_level2/n_lcm.js
@@ -9,3 +9,36 @@ function gcd(a, b) {
 function solution(arr) {
   return arr.reduce((a, b) => (a * b) / getGcd(a, b));
 }
+
+// Other solution 1
+function nlcm(num) {
+  var answer = 0;
+
+  function gcd(a, b) {
+    if (!b) return a;
+    return gcd(b, a % b);
+  }
+
+  function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+  }
+
+  answer = num.reduce(function (a, b) {
+    var min = Math.min(a, b);
+    var max = Math.max(a, b);
+    return lcm(min, max);
+  });
+  return answer;
+}
+
+// Other solution 2
+function solution(arr) {
+  return arr.reduce((acc, cur) => {
+    const recursive = (min, max) => {
+      return min % max === 0 ? max : recursive(max, min % max);
+    };
+
+    let max = 0;
+    return (acc * cur) / recursive(acc, cur);
+  });
+}
