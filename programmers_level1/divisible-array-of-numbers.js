e1110fccb33dde79e807ccb54326cb040c782fea
Jay <jay.lee.2280@gmail.com>
2021-11-12 12:54:04 +0900

    📝 Another way with map

--- programmers_level1/divisible-array-of-numbers.js
+++ programmers_level1/divisible-array-of-numbers.js
@@ -1,8 +1,15 @@
-// 2021-10-10
+// 2021-10-10 & 2021-11-12
 // 나누어 떨어지는 숫자 배열
 
 // My Solution
 function solution(arr, divisor) {
-  let newarr = arr.filter(e => e % divisor === 0);
-  return newarr.length === 0 ? [-1] : newarr.sort((a, b) => a - b);
+  const filtered = arr.filter(e => e % divisor === 0);
+  return filtered.length ? filtered.sort((a, b) => a - b) : [-1];
+}
+
+// Another way (with map)
+function solution(arr, divisor) {
+  let filtered = [];
+  arr.map(v => v % divisor === 0 && filtered.push(v));
+  return filtered.length ? filtered.sort((a, b) => a - b) : [-1];
 }
