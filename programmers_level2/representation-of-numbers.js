921b37de802cb370f1cc710c6d3dce9bb0e96388
Jay <jay.lee.2280@gmail.com>
2021-11-14 10:28:45 +0900

    ➕ Other's solutions(2)

--- programmers_level2/representation-of-numbers.js
+++ programmers_level2/representation-of-numbers.js
@@ -17,3 +17,31 @@ function solution(n) {
   }
   return answer;
 }
+
+// Other's solution 1
+// Find a number that is both odd and divisor of a given number.
+// All odd numbers can be expressed as the sum of consecutive natural numbers.
+function expressions(num) {
+  let answer = 0;
+
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0 && i % 2 === 1) answer++;
+  }
+  return answer;
+}
+
+// Other's solution 2 - Recursion
+function solution(n) {
+  let answer = 0;
+
+  function re(current, sum) {
+    if (sum === n) return true;
+    if (sum > n) return false;
+    return re(current + 1, sum + current);
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (re(i, 0)) answer++;
+  }
+  return answer;
+}
