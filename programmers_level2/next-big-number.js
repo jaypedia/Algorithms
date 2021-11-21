8c257bcb6802c718c2aabcf0905490dae806cd15
Jay <jay.lee.2280@gmail.com>
2021-11-21 15:29:54 +0900

    ➕ Update solution

--- programmers_level2/next-big-number.js
+++ programmers_level2/next-big-number.js
@@ -1,4 +1,4 @@
-// 2021-11-14
+// 2021-11-14 & 11-21
 // 다음 큰 숫자
 
 // My solution
@@ -13,4 +13,22 @@ function solution(n) {
   for (let i = n + 1; ; i++) {
     if (numberOfOne(n) === numberOfOne(i)) return i;
   }
+}
+
+// Use Regex and while
+function one(n) {
+  return n.toString(2).match(/1/g).length;
+}
+
+function nextBig(n) {
+  let i = n + 1;
+  while (one(n) !== one(i)) {
+    i++;
+  }
+  return i;
+}
+
+// Recursive function
+function solution(n, a = n + 1) {
+  return one(n) === one(a) ? a : solution(n, a + 1);
 }
