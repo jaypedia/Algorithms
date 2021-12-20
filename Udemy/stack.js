dda1b7d60a9efa6c8979ba8d63998f195b980569
Jay <jay.lee.2280@gmail.com>
2021-12-20 20:15:55 +0900

    Stack

--- Udemy/stack.js
+++ Udemy/stack.js
@@ -1,5 +1,5 @@
 /* Stack
-    2021.12.17~
+    2021.12.17~20
 
     Linked List 활용하여 Stack을 구현하기 
 */
@@ -7,7 +7,7 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = next;
+    this.next = null;
   }
 }
 
@@ -16,5 +16,29 @@ class Stack {
     this.first = null;
     this.last = null;
     this.size = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
   }
 }
