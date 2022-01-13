30e68610c819d2a852507cf5c4e2ef1d4d0d86af
Jay <jay.lee.2280@gmail.com>
2022-01-13 19:24:37 +0900

    LeetCode : Review reverse integer

--- leetcode/reverse-integer.js
+++ leetcode/reverse-integer.js
@@ -78,3 +78,11 @@ const reverse = int => {
   }
   return reversed > 2 ** 31 - 1 || reversed < -(2 ** 31) ? 0 : reversed;
 };
+
+// Another solution
+const reverse = x => {
+  const isMinus = x < 0 ? true : false;
+  const reverseX = [...(Math.abs(x) + '')].reverse().join('');
+  const answer = isMinus ? -reverseX : +reverseX;
+  return answer <= 2 ** 31 - 1 && answer >= -(2 ** 31) ? answer : 0;
+};
