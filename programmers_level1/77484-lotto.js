605a7b8ad1f57620118c9cfa5c0976fdf31d28a5
Millie <jay.lee.2280@gmail.com>
2022-03-02 23:46:37 +0900

    Add other's solutions: use filter()

--- programmers_level1/77484-lotto.js
+++ programmers_level1/77484-lotto.js
@@ -5,8 +5,8 @@
 // My solution
 function solution(lottos, win_nums) {
   let i = 0;
-  let count = 0;
-  let zeros = 0;
+  let count = 0; // 번호 일치하는 개수 카운트
+  let zeros = 0; // 0의 개수 카운트
   while (i < lottos.length) {
     if (lottos[i] === 0) {
       zeros++;
@@ -19,7 +19,30 @@ function solution(lottos, win_nums) {
 
   const rank = [6, 6, 5, 4, 3, 2, 1];
 
-  return [rank[count + zeros], rank[count]];
+  return [rank[count + zeros], rank[count]]; // [최고 순위, 최저 순위]
 }
 
 console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]));
+
+// Other's solutions
+
+function solution(lottos, win_nums) {
+  const rank = [6, 6, 5, 4, 3, 2, 1];
+
+  const minCount = lottos.filter(v => win_nums.includes(v)).length;
+  const zeroCount = lottos.filter(v => !v).length;
+  const maxCount = minCount + zeroCount;
+
+  return [rank[maxCount], rank[minCount]];
+}
+
+function solution(lottos, win_nums) {
+  const answer = [];
+  const min = lottos.filter(n => win_nums.includes(n)).length;
+  const max = lottos.filter(n => n === 0).length + min;
+
+  max > 1 ? answer.push(7 - max) : answer.push(6);
+  min > 1 ? answer.push(7 - min) : answer.push(6);
+
+  return answer;
+}
