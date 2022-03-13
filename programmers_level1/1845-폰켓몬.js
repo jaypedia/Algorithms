e65792c72cd05f5c4faaa0282d361a32e53c3605
Millie <jay.lee.2280@gmail.com>
2022-03-13 23:32:37 +0900

    Add other's solutions: use Math.min, object

--- programmers_level1/1845-폰켓몬.js
+++ programmers_level1/1845-폰켓몬.js
@@ -8,9 +8,24 @@ function solution(nums) {
   return set.size <= max ? set.size : max;
 }
 
+// My another solution
+var solution = nums => Math.min(new Set(nums).size, nums.length / 2);
+
 // Other's solution
 function solution(nums) {
   const max = nums.length / 2;
   const arr = [...new Set(nums)];
   return arr.length > max ? max : arr.length;
 }
+
+// Without Set (by Hemdi)
+function hemdiSolution(nums) {
+  const max = nums.length / 2;
+  const obj = {};
+  let objKeyCount;
+  nums.forEach(n => (obj[n] = (obj[n] || 0) + 1));
+  objKeyCount = Object.keys(obj).length;
+  return objKeyCount <= max ? objKeyCount : max;
+}
+
+console.log(hemdiSolution([3, 3, 3, 2, 2, 4]));
