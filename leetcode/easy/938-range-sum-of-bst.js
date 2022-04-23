29bd841653171fd8a58e78f9df3b8884a95c0aca
Millie <jay.lee.2280@gmail.com>
2022-04-23 20:43:46 +0900

    Solve LeetCode: Range sum of BST

---
+++ leetcode/easy/938-range-sum-of-bst.js
@@ -0,0 +1,39 @@
+// April 4, 2022
+// https://leetcode.com/problems/range-sum-of-bst/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+  let answer = 0;
+  let current = root;
+
+  const traverse = node => {
+    if (n >= low && n <= high) answer += node.val;
+    if (node.left) traverse(node.left);
+    if (node.right) traverse(node.right);
+  };
+
+  traverse(current);
+
+  return answer;
+};
+
+// https://leetcode.com/problems/range-sum-of-bst/discuss/374212/Javascript-solution
+var rangeSumBST = (root, low, high) => {
+  if (!root) return 0;
+  else if (root.val < low) return rangeSumBST(root.right, low, high);
+  else if (root.val > high) return rangeSumBST(root.left, low, high);
+  else return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+};
