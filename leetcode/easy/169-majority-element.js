3a00095287365f47b63ce24cf50d2855fa77c4df
Millie <jay.lee.2280@gmail.com>
2022-05-29 21:12:42 +0900

    Add solutions to Majority Element

--- leetcode/easy/169-majority-element.js
+++ leetcode/easy/169-majority-element.js
@@ -8,7 +8,7 @@
  */
 var majorityElement = function (nums) {
   nums.sort((a, b) => a - b);
-  const count = Math.ceil(nums.length / 2);
+  const majorityCount = Math.ceil(nums.length / 2);
   let numCount = 0;
   let majority = null;
 
@@ -20,10 +20,55 @@ var majorityElement = function (nums) {
       numCount++;
     }
 
-    if (numCount >= count) {
+    if (numCount >= majorityCount) {
       return majority;
     }
   }
 
   return majority;
 };
+
+// use while
+const majorityElement2 = nums => {
+  nums.sort((a, b) => a - b);
+  const majorityCount = Math.ceil(nums.length / 2);
+  let numCount = 0;
+  let majority = null;
+  let i = 0;
+
+  while (numCount < majorityCount) {
+    if (majority === nums[i]) {
+      numCount++;
+    } else {
+      majority = nums[i];
+      numCount = 1;
+    }
+    i++;
+  }
+
+  return majority;
+};
+
+// use Map
+const majorityElementMap = nums => {
+  const map = new Map();
+  const majorityCount = Math.ceil(nums.length / 2);
+
+  nums.forEach(n => {
+    map.has(n) ? map.set(n, map.get(n) + 1) : map.set(n, 1);
+  });
+
+  for (const entry of map) {
+    if (entry[1] >= majorityCount) {
+      return entry[0];
+    }
+  }
+};
+
+const majorityElement3 = nums => {
+  nums.sort((a, b) => a - b);
+  return nums[Math.floor(nums.length / 2)];
+};
+
+console.log(majorityElement3([3, 2, 3]));
+console.log(majorityElement3([1]));
