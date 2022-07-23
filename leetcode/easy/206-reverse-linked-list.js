078eab966da812456d20c2a8dca38c8a148f8d05
Millie <jay.lee.2280@gmail.com>
2022-07-23 22:25:14 +0900

    Add solutions to LeetCode: Reverse Linked List

--- leetcode/easy/206-reverse-linked-list.js
+++ leetcode/easy/206-reverse-linked-list.js
@@ -1,7 +1,8 @@
 // July 7, 2022
 // https://leetcode.com/problems/reverse-linked-list/submissions/
 
-const reverseList = head => {
+// Iterative solution
+var reverseList = head => {
   let prevNode = null;
 
   while (head) {
@@ -11,4 +12,17 @@ const reverseList = head => {
     head = nextNode;
   }
   return prevNode;
+};
+
+// July 23, 2022
+// Recursive solution
+var reverseList = head => {
+  const helper = (head, newHead) => {
+    if (!head) return newHead;
+    const nextNode = head.next;
+    head.next = newHead;
+    return helper(nextNode, head);
+  };
+
+  return helper(head, null);
 };
