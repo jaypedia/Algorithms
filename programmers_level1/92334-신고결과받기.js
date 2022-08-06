672db7b740aec4e288d284186cf2d8c285e80d9b
Millie <jay.lee.2280@gmail.com>
2022-08-06 12:05:38 +0900

    Add solutions to Programmers Level 1: 신고 결과 받기

--- programmers_level1/92334-신고결과받기.js
+++ programmers_level1/92334-신고결과받기.js
@@ -33,6 +33,35 @@ const checkSuspended = (reportSet, k) => {
   return suspendedIdSet;
 };
 
+// Aug 6, 2022
+function solution(id_list, report, k) {
+  const reportList = [...new Set(report)];
+  const reportObj = {};
+  const reportCountMap = new Map();
+  const suspendedUser = new Set();
+
+  reportList.forEach(v => {
+    const [a, b] = v.split(' ');
+    reportCountMap.set(b, (reportCountMap.get(b) || 0) + 1);
+    if (reportCountMap.get(b) + 1 > k) suspendedUser.add(b);
+    if (reportObj[a]) reportObj[a].push(b);
+    else reportObj[a] = [b];
+  });
+
+  const answer = [];
+  id_list.forEach(id => {
+    let count = 0;
+    const reported = reportObj[id];
+    if (reported) {
+      reported.forEach(r => {
+        if (suspendedUser.has(r)) count++;
+      });
+    }
+    answer.push(count);
+  });
+  return answer;
+}
+
 console.log(
   solution(
     ['muzi', 'frodo', 'apeach', 'neo'],
