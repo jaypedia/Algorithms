7236c5a4f0032ec4e9af84739e17bbc26439a632
Millie <jay.lee.2280@gmail.com>
2022-09-29 16:34:18 +0900

    Add solutions to LeetCode: Linked List Cycle

--- leetcode/easy/141-linked-list-cycle.js
+++ leetcode/easy/141-linked-list-cycle.js
@@ -28,3 +28,28 @@ var hasCycle = function (head) {
     if (visited.includes(head)) return true;
   }
 };
+
+// Sep 29, 2022
+var hasCycle = function (head) {
+  const set = new Set();
+  while (head) {
+    set.add(head);
+    head = head.next;
+    if (!head) return false;
+    if (set.has(head)) return true;
+  }
+
+  return false;
+};
+
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+
+  return false;
+};
