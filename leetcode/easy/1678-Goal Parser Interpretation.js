7838c1251426760bc5af5861b08caeea4121b896
Millie <jay.lee.2280@gmail.com>
2022-10-12 00:18:40 +0900

    Solve LeetCode: Goal Parser Interpretation

---
+++ leetcode/easy/1678-Goal Parser Interpretation.js
@@ -0,0 +1,25 @@
+// Oct 12, 2022
+// https://leetcode.com/problems/goal-parser-interpretation/description/
+
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+  let answer = '';
+  for (let i = 0; i < command.length; ) {
+    if (command[i] === 'G') {
+      answer += 'G';
+      i++;
+      continue;
+    }
+    if (command[i + 1] === ')') {
+      answer += 'o';
+      i += 2;
+      continue;
+    }
+    answer += 'al';
+    i += 4;
+  }
+  return answer;
+};
