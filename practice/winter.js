77d0f132b95a0430c1e03f774875f2a0ea74700e
Millie <jay.lee.2280@gmail.com>
2022-11-07 15:38:48 +0900

    Practice solution

--- practice/winter.js
+++ practice/winter.js
@@ -1,5 +1,4 @@
 // Nov 5, 2022
-
 function solution(line) {
   let answer = '';
 
@@ -22,3 +21,46 @@ function solution(line) {
 
   return answer;
 }
+
+// Nov 7, 2022
+function solution2(n, student, point) {
+  const arr = [];
+  let answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    arr.push({ id: i, score: 0, isA: i <= n / 2 ? true : false });
+  }
+
+  const getAMin = () => {
+    let aMin = Infinity;
+    let student = null;
+    arr.forEach(v => {
+      if (!v.isA) return;
+      if (v.score < aMin || (v.score === aMin && v.id > student.id)) {
+        aMin = v.score;
+        student = v;
+      }
+    });
+    return student;
+  };
+
+  student.forEach((s, i) => {
+    const aMinStudent = getAMin();
+    const cur = arr.find(v => v.id === s);
+    cur.score += point[i];
+    if (cur.isA) return;
+    if (cur.score > aMinStudent.score || (cur.score === aMinStudent.score && cur.id < aMinStudent.id)) {
+      cur.isA = true;
+      aMinStudent.isA = false;
+      answer++;
+    }
+  });
+
+  return answer;
+}
+
+const n = 6;
+const student = [6, 1, 4, 2, 5, 1, 3, 3, 1, 6, 5];
+const point = [3, 2, 5, 3, 4, 2, 4, 2, 3, 2, 2];
+
+console.log(solution2(n, student, point)); // 7
