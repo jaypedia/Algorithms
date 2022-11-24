ee98adc416422e475299ae5336839a08594cc717
Millie <jay.lee.2280@gmail.com>
2022-11-24 18:25:08 +0900

    Solve Programmers Level 2: 다리를 지나는 트럭

--- programmers_level2/42583-다리를지나는트럭.js
+++ programmers_level2/42583-다리를지나는트럭.js
@@ -25,3 +25,28 @@ function solution(bridge_length, weight, truck_weights) {
 
   return time;
 }
+
+// Nov 24, 2022
+function solution(bridge_length, weight, truck_weights) {
+  let answer = 1;
+  let bridgeWeight = truck_weights[0];
+  const curBridge = [{ truckWeight: truck_weights.shift(), second: 0 }];
+
+  while (truck_weights.length || curBridge.length) {
+    const curTruck = truck_weights[0];
+    if (bridge_length > curBridge.length && bridgeWeight + curTruck <= weight) {
+      const truckWeight = truck_weights.shift();
+      curBridge.push({ truckWeight, second: 0 });
+      bridgeWeight += truckWeight;
+    }
+    curBridge.map(truck => truck.second++);
+    if (curBridge[0].second >= bridge_length) {
+      bridgeWeight -= curBridge[0].truckWeight;
+      curBridge.shift();
+    }
+    answer++;
+  }
+  return answer;
+}
+
+console.log(solution(2, 10, [7, 4, 5, 6]));
