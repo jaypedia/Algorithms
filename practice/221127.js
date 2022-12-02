3981a9bb399d80561d200b7396ea4f2d2326fc8b
Millie <jay.lee.2280@gmail.com>
2022-12-02 14:09:54 +0900

    Practice solution

--- practice/221127.js
+++ practice/221127.js
@@ -1,3 +1,4 @@
+// #4
 function solution(id) {
   const len = id.length;
   const maskingCount = len % 2 ? (len - 1) / 2 : len / 2;
@@ -7,4 +8,35 @@ function solution(id) {
   const endCount = restCount - startCount;
 
   return id.slice(0, startCount) + masking + id.slice(-endCount);
+}
+
+// #5
+function getStylesOf(element, styles) {
+  const elementType = element.elementType;
+  const obj = styles[elementType] || {};
+  const stylesKeyArr = Object.keys(styles);
+
+  for (const className of stylesKeyArr) {
+    if (className.slice(0, 1) !== '.') continue;
+    const classObj = styles[className];
+    for (const key in classObj) {
+      obj[key] = classObj[key];
+    }
+  }
+
+  const id = '#' + element.id;
+  const idObj = styles[id];
+
+  for (const key in idObj) {
+    obj[key] = idObj[key];
+  }
+
+  return obj;
+}
+
+function solution(_element, _styles) {
+  const element = JSON.parse(_element);
+  const styles = JSON.parse(_styles);
+  const result = getStylesOf(element, styles);
+  return JSON.stringify(result, Object.keys(result).sort());
 }
