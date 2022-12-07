b17f629aeeae36484a8f043e808872a2a32704d4
Millie <jay.lee.2280@gmail.com>
2022-12-07 22:42:49 +0900

    Solve Programmers Level 1: 기사단원의 무기

--- programmers_level1/136798-기사단원의 무기.js
+++ programmers_level1/136798-기사단원의 무기.js
@@ -25,3 +25,31 @@ function solution(number, limit, power) {
 
   return answer;
 }
+
+// 통과한 풀이
+function solution(number, limit, power) {
+  let answer = 0;
+
+  // https://www.geeksforgeeks.org/count-divisors-n-on13/
+  const countDivisors = num => {
+    let count = 0;
+    for (let i = 1; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        if (num / i === i) count++; // If divisors are equal, count only one
+        else count = count + 2; // Otherwise count both
+      }
+    }
+    return count;
+  };
+
+  for (let i = 1; i <= number; i++) {
+    const numberOfDivisors = countDivisors(i);
+    if (numberOfDivisors > limit) {
+      answer += power;
+    } else {
+      answer += numberOfDivisors;
+    }
+  }
+
+  return answer;
+}
