f8a63ea343260345aa0ab171454aaa53913a7a9d
Millie <jay.lee.2280@gmail.com>
2023-01-03 10:13:32 +0900

    Solve LeetCode: 2032. Two Out of Three

---
+++ leetcode/easy/2032. Two Out of Three.js
@@ -0,0 +1,45 @@
+// Jan 3, 2023
+// https://leetcode.com/problems/two-out-of-three/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @return {number[]}
+ */
+var twoOutOfThree = function (nums1, nums2, nums3) {
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+  const set3 = new Set(nums3);
+
+  const map = [...set1, ...set2, ...set3].reduce((acc, cur) => {
+    acc.set(cur, ~~acc.get(cur) + 1);
+    return acc;
+  }, new Map());
+  const answer = [];
+
+  for (const [key, value] of map) {
+    if (value >= 2) {
+      answer.push(key);
+    }
+  }
+  return answer;
+};
+
+// https://leetcode.com/problems/two-out-of-three/solutions/1982928/very-easy-javascript-solution-with-using-very-less-space/
+var twoOutOfThree = function (nums1, nums2, nums3) {
+  let set = new Set(nums1),
+    res = new Set();
+  nums1 = new Set();
+
+  for (let num of nums2) {
+    if (set.has(num)) res.add(num);
+    nums1.add(num);
+  }
+
+  for (let num of nums3) {
+    if (set.has(num) || nums1.has(num)) res.add(num);
+  }
+
+  return [...res];
+};
