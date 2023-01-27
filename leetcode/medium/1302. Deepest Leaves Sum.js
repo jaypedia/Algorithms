5cf50e4270c2b4d23cda6eba67f97a4b0243b9d9
Millie <jay.lee.2280@gmail.com>
2023-01-27 23:49:06 +0900

    Solve LeetCode: 1302. Deepest Leaves Sum

--- leetcode/medium/1302. Deepest Leaves Sum.js
+++ leetcode/medium/1302. Deepest Leaves Sum.js
@@ -1,4 +1,4 @@
-// Jan 21~22, 2023
+// Jan 21~25, 2023
 // https://leetcode.com/problems/deepest-leaves-sum/
 
 // BFS
@@ -18,5 +18,28 @@ var deepestLeavesSum = function (root) {
       if (node.right) queue.push(node.right);
     }
   }
+  return sum;
+};
+
+// DFS
+var deepestLeavesSum = function (root) {
+  if (!root) return 0;
+  let sum = 0;
+  let maxLevel = 0;
+
+  const calculateSumAtLevel = (node, level) => {
+    if (!node) return;
+    if (level > maxLevel) {
+      sum = 0;
+      maxLevel = level;
+    }
+    if (level === maxLevel) {
+      sum += node.val;
+    }
+
+    calculateSumAtLevel(node.left, level + 1);
+    calculateSumAtLevel(node.right, level + 1);
+  };
+  calculateSumAtLevel(root, 0);
   return sum;
 };
