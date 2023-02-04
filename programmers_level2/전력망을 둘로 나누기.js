b6a6c4ba176cc0b5708845668dc6b48f86e9f0e0
Millie <jay.lee.2280@gmail.com>
2023-02-04 21:29:03 +0900

    Solve Programmers Level 2: 전력망을 둘로 나누기

--- programmers_level2/전력망을 둘로 나누기.js
+++ programmers_level2/전력망을 둘로 나누기.js
@@ -5,7 +5,7 @@
 function solution(n, wires) {
   const obj = {};
 
-  wires.forEach(w => {
+  wires.forEach((w) => {
     const [n1, n2] = w;
     obj[n1] = obj[n1] ? obj[n1] + 1 : 1;
     obj[n2] = obj[n2] ? obj[n2] + 1 : 1;
@@ -13,12 +13,12 @@ function solution(n, wires) {
 
   const maxCount = Math.max(...Object.values(obj));
   const keys = Object.keys(obj);
-  const root = keys.find(key => obj[key] === maxCount);
+  const root = keys.find((key) => obj[key] === maxCount);
   delete obj[root];
   const secondMaxCount = Math.max(...Object.values(obj));
-  const node = keys.find(key => obj[key] === secondMaxCount);
+  const node = keys.find((key) => obj[key] === secondMaxCount);
 
-  const getNodeIdx = wires => {
+  const getNodeIdx = (wires) => {
     for (let i = 0; i < wires.length; i++) {
       if (wires[i].includes(root) && wires[i].includes(node)) {
         return i;
@@ -31,57 +31,57 @@ function solution(n, wires) {
 }
 
 // Reference: https://dev-note-97.tistory.com/308
+// Review: Feb 4, 2023
 function solution2(n, wires) {
-  const makeLinksObj = wires => {
-    const links = {};
-    wires.map(w => {
-      const [a, b] = w;
-      if (!links[a]) links[a] = [];
-      if (!links[b]) links[b] = [];
-      links[a].push(b);
-      links[b].push(a);
-    });
-    return links;
-  };
+  const links = {};
+  wires.map((wire) => {
+    const [node1, node2] = wire;
+    if (!links[node1]) links[node1] = [];
+    if (!links[node2]) links[node2] = [];
+    links[node1].push(node2);
+    links[node2].push(node1);
+  });
 
-  const searchTree = (root, exception) => {
+  const countNode = (root, exception) => {
     let count = 0;
     const queue = [root];
     const visited = [];
     visited[root] = true;
 
     while (queue.length) {
+      count++;
       const cur = queue.pop();
-      links[cur].map(next => {
-        if (next !== exception && !visited[next]) {
-          visited[next] = true;
-          queue.push(next);
+      links[cur].forEach((node) => {
+        if (node !== exception && visited[node] === undefined) {
+          visited[node] = true;
+          queue.push(node);
         }
       });
-      count++;
     }
 
     return count;
   };
 
-  const links = makeLinksObj(wires);
-  console.log(links);
   let answer = 98;
-  wires.map((w, _) => {
-    const [a, b] = w;
-    const dif = Math.abs(searchTree(a, b) - searchTree(b, a));
-    answer = answer > dif ? dif : answer;
+
+  wires.map((wire) => {
+    const [node1, node2] = wire;
+    const diff = Math.abs(countNode(node1, node2) - countNode(node2, node1));
+    answer = answer > diff ? diff : answer;
   });
+
   return answer;
 }
 
 console.log(
-  solution2(7, [
-    [1, 2],
-    [2, 7],
-    [3, 7],
+  solution2(9, [
+    [1, 3],
+    [2, 3],
     [3, 4],
     [4, 5],
-    [6, 7],
+    [4, 6],
+    [4, 7],
+    [7, 8],
+    [7, 9],
   ])
 );
