a8b642679d611be787539ccc817a22079df137d3
Millie <jay.lee.2280@gmail.com>
2023-02-11 18:40:28 +0900

    Remove folders

--- codility/lessons/1-BinaryGap.js
+++
@@ -1,8 +0,0 @@
-// May 8, 2022
-
-function solution(N) {
-  const binaryStr = N.toString(2);
-  const arr = binaryStr.split('1').map(v => v.length);
-  arr.pop();
-  return Math.max(...arr) === binaryStr.length - 1 ? 0 : Math.max(...arr);
-}
