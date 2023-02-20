12e643febf8cbeb945284467f3a247a5eee537a3
Millie <jay.lee.2280@gmail.com>
2023-02-20 20:43:21 +0900

    Move folders

--- Programmers/Level1/자릿수 더하기.js
+++
@@ -1,6 +0,0 @@
-// 2021-10-08
-// https://school.programmers.co.kr/learn/courses/30/lessons/12931
-
-function solution(n) {
-  return (n + '').split('').reduce((acc, cur) => +acc + +cur, 0);
-}
--- leetcode/easy/1684. Count the Number of Consistent Strings.js
+++
@@ -1,9 +0,0 @@
-// Oct 28, 2022
-// https://leetcode.com/problems/count-the-number-of-consistent-strings/
-
-var countConsistentStrings = function (allowed, words) {
-  const set = new Set(allowed);
-  return words.reduce((acc, cur) => {
-    return cur.split('').every(v => set.has(v)) ? ++acc : acc;
-  }, 0);
-};
