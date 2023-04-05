276d95185c905504000556f86acc45d3a3c797a0
Jayun <jay.lee.2280@gmail.com>
2023-04-06 02:03:08 +0900

    Review Programmers Level 2: 카펫

---
+++ platforms/programmers/level2/42842-카펫.js
@@ -0,0 +1,45 @@
+// May 1, 2022
+// https://programmers.co.kr/learn/courses/30/lessons/42842
+
+function solution(brown, yellow) {
+  const total = brown + yellow;
+
+  for (let height = 3; height <= total; height++) {
+    const width = total / height;
+    if (!Number.isInteger(width)) continue;
+
+    const yellowCount = total - (width * 2 + (height - 2) * 2);
+    if (yellowCount === yellow) {
+      return [width, height];
+    }
+  }
+}
+
+function solution(brown, yellow) {
+  const total = brown + yellow;
+
+  for (let i = Math.floor(total / 2); i > 0; i--) {
+    if (total % i !== 0) continue;
+
+    const width = i;
+    const height = total / i;
+
+    if ((width - 2) * (height - 2) === yellow) {
+      return [width, height];
+    }
+  }
+}
+
+// Sep 4, 2022
+function solution(brown, yellow) {
+  const sum = brown + yellow;
+  for (let h = 3; h <= sum / h; h++) {
+    const w = sum / h;
+    if (Number.isInteger(w)) {
+      const outline = 2 * (h + w) - 4;
+      if (outline === brown) {
+        return [w, h];
+      }
+    }
+  }
+}
