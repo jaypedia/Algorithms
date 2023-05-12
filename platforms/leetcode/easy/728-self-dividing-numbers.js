5d9116a0ed9e866029843a205b00192a0b056acd
Jayun <jay.lee.2280@gmail.com>
2023-05-12 22:48:27 +0900

    Review LeetCode: 728. Self Dividing Numbers

---
+++ platforms/leetcode/easy/728-self-dividing-numbers.js
@@ -0,0 +1,26 @@
+// June 25, 2022
+// https://leetcode.com/problems/self-dividing-numbers/
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function (left, right) {
+  const answer = [];
+
+  for (let i = left; i <= right; i++) {
+    let isSelf = true;
+    const cur = i + '';
+
+    for (let j = 0; j < cur.length; j++) {
+      if (cur[j] === '0' || i % +cur[j]) {
+        isSelf = false;
+        break;
+      }
+    }
+    if (isSelf) answer.push(i);
+  }
+
+  return answer;
+};
