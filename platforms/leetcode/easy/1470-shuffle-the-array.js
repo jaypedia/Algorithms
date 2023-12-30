95dc74b94d5c76f53a9574ebdd125efd75a71f67
Jayun <jay.lee.2280@gmail.com>
2023-12-30 23:41:17 +0900

    Solve LeetCode: 1470. Shuffle the Array

---
+++ platforms/leetcode/easy/1470-shuffle-the-array.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/shuffle-the-array/description/
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  const answer = [];
+  for (let i = 0; i < n; i++) {
+    answer.push(nums[i], nums[n + i]);
+  }
+  return answer;
+};
